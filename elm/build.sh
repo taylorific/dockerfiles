c961999d30e38b3fb3dfc4da842f9265fca1e633
Mischa Taylor <sheska@linux.com>
2019-11-24 22:33:52 -0800

    Add elm image

--- elm/build.sh
+++ elm/build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-docker build -t us.gcr.io/taylorific/elm .
+docker build -t us.gcr.io/taylorific/elm:0.19.1 .
--- elm/deliver.sh
+++ elm/deliver.sh
@@ -1,7 +1,4 @@
 #!/bin/sh
 
 echo \
-  'us.gcr.io/taylorific/node:13.1.0-bullseye' \
-  'us.gcr.io/taylorific/node:13.1-bullseye' \
-  'us.gcr.io/taylorific/node:13-bullseye' \
-  'us.gcr.io/taylorific/node:bullseye' | xargs -n 1 docker push
+  'us.gcr.io/taylorific/elm:0.19.1' | xargs -n 1 docker push
